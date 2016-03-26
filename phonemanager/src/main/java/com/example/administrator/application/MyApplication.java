c2bb77530d81ddfb40ba1a89f256e519cca0d3ca
zhai <1689814801@qq.com>
2016-03-26 19:48:09 +0800

    设置了防盗功能的密码页面，以及加了存储密码的加密算法

--- phonemanager/src/main/java/com/example/administrator/application/MyApplication.java
+++ phonemanager/src/main/java/com/example/administrator/application/MyApplication.java
@@ -10,11 +10,18 @@ import android.content.SharedPreferences;
 public class MyApplication extends Application {
     public static String SERVER_PATH;//定义了从服务器上获取更新文件的路径 里面包含URL
     public static SharedPreferences configsp;//sharedpreferences 为一个轻量级的数据库奥 亲
-//    192.168.3.31/Aday10Network
+    public static SharedPreferences.Editor editor;
     @Override
     public void onCreate() {
-        super.onCreate();http://192.168.3.31/Aday10Network/version.json
+        super.onCreate();
         SERVER_PATH = "http://192.168.3.31/Aday10Network/";
         configsp = getSharedPreferences("config", MODE_PRIVATE);
+        editor=configsp.edit();
+    }
+    //设置数据并销毁这个界面
+    public static void setConfigValue(String key, String value){
+
+        editor.putString(key,value);
+        editor.commit();
     }
 }
---
+++ phonemanager/src/main/java/com/example/administrator/phonemanager/PhoneSafeActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.phonemanager;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class PhoneSafeActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_safe);
+    }
+}
